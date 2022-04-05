6327167aa1c95f6700937cf857c2277bda363705
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-04-04 22:01:38 -0300

    import the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import './App.css'
+import styled from 'styled-components'
+import WhatsApp from './components/WhatsApp';
 
 const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <StyledApp className="App">
+      <WhatsApp />
+    </StyledApp>
   )
 }
 
 export default App;
+
+const StyledApp = styled.div`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100vh;
+  font-family: Arial, Helvetica, sans-serif;
+
+  background-color: #0A1014;
+`
